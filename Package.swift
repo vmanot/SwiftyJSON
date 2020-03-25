924883b7f440d7409acf74f14d5b3bf0224fed0a
Vatsal Manot <vatsal.manot@yahoo.com>
2020-03-25 15:59:54 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
     ],
     products: [
-        .library(name: "SwiftyJSON", targets: ["SwiftyJSON"])
+        .library(name: "SwiftyJSON", type: .dynamic, targets: ["SwiftyJSON"])
     ],
     targets: [
         .target(name: "SwiftyJSON", dependencies: []),
